ceec4100f1335a02f6a9bb556904a906229aaf73
MuratCiftci <mciftci174@posta.pau.edu.tr>
2023-03-12 19:30:27 +0300

    change: font family to inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ['"Inter var"', ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {

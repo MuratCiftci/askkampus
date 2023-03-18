3deef6fab688c49230c2e67d68bcba57c57a985a
MuratCiftci <mciftci174@posta.pau.edu.tr>
2023-03-18 14:34:07 +0300

    feature: dark mode added.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,115 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { fontFamily } = require("tailwindcss/defaultTheme");
+const { spacing, fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class", '[data-theme="dark"]'],
+  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "blue-opaque": "rgb(13 42 148 / 18%)",
+        gray: {
+          0: "#fff",
+          100: "#fafafa",
+          200: "#eaeaea",
+          300: "#999999",
+          400: "#888888",
+          500: "#666666",
+          600: "#444444",
+          700: "#333333",
+          800: "#222222",
+          900: "#111111",
+        },
+      },
       fontFamily: {
-        sans: ['"Inter var"', ...fontFamily.sans],
+        sans: ["IBM Plex Sans", ...fontFamily.sans],
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
+
+      typography: (/** @type {(arg0: string) => any} */ theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.700"),
+            a: {
+              color: theme("colors.blue.500"),
+              "&:hover": {
+                color: theme("colors.blue.700"),
+              },
+              code: { color: theme("colors.blue.400") },
+            },
+            "h2,h3,h4": {
+              "scroll-margin-top": spacing[32],
+            },
+            thead: {
+              borderBottomColor: theme("colors.gray.200"),
+            },
+            code: { color: theme("colors.pink.500") },
+            "blockquote p:first-of-type::before": false,
+            "blockquote p:last-of-type::after": false,
+            keyframes: {
+              "accordion-down": {
+                from: { height: 0 },
+                to: { height: "var(--radix-accordion-content-height)" },
+              },
+              "accordion-up": {
+                from: { height: "var(--radix-accordion-content-height)" },
+                to: { height: 0 },
+              },
+            },
+            animation: {
+              "accordion-down": "accordion-down 0.2s ease-out",
+              "accordion-up": "accordion-up 0.2s ease-out",
+            },
+          },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+        dark: {
+          css: {
+            color: theme("colors.gray.200"),
+            a: {
+              color: theme("colors.blue.400"),
+              "&:hover": {
+                color: theme("colors.blue.600"),
+              },
+              code: { color: theme("colors.blue.400") },
+            },
+            blockquote: {
+              borderLeftColor: theme("colors.gray.700"),
+              color: theme("colors.gray.300"),
+            },
+            "h2,h3,h4": {
+              color: theme("colors.gray.100"),
+              "scroll-margin-top": spacing[32],
+            },
+            hr: { borderColor: theme("colors.gray.700") },
+            ol: {
+              li: {
+                "&:before": { color: theme("colors.gray.500") },
+              },
+            },
+            ul: {
+              li: {
+                "&:before": { backgroundColor: theme("colors.gray.500") },
+              },
+            },
+            strong: { color: theme("colors.gray.100") },
+            thead: {
+              th: {
+                color: theme("colors.gray.100"),
+              },
+              borderBottomColor: theme("colors.gray.600"),
+            },
+            tbody: {
+              tr: {
+                borderBottomColor: theme("colors.gray.700"),
+              },
+            },
+          },
         },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      }),
     },
+  },
+  variants: {
+    typography: ["dark"],
   },
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore

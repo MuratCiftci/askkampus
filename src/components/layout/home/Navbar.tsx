985de8c409786482b2c26c3e9176f4aa7996f7f7
MuratCiftci <mciftci174@posta.pau.edu.tr>
2023-03-18 14:34:46 +0300

    feature : add navbar

--- src/components/layout/home/Navbar.tsx
+++ src/components/layout/home/Navbar.tsx
@@ -1,19 +1,72 @@
-import Image from 'next/image'
-import React from 'react'
+import React from "react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import Image from "next/image";
+import { Input } from "~/components/shared/ui/Input";
+import { Button } from "~/components/shared/ui/Button";
 
 const Navbar = () => {
+  const { theme, setTheme } = useTheme();
   return (
-    <div className="flex flex-row justify-between items-center bg-white shadow-md px-8 fixed h-16 w-full top-0 left-0">
-         <Image src="/logo.png" width={100} height={50} alt="logo" />
+    <header className="z-80 fixed top-0 left-0 mb-2 flex h-12 w-full flex-row items-center">
+      <div className="box-border inline-flex grow flex-row items-center px-5 ">
+        <Link href="/" className="w-64 align-middle mx-auto pl-4">
+         
+      
+            <Image 
+              src={theme === 'dark' ? "/images/logo-dark.png" : "/images/logo-white.png"}
+              alt="logo"
+              width={200}
+              height={48}
 
-            <div className="flex flex-row gap-4">
-                <a href="#" className="text-gray-500 hover:text-gray-900">Home</a>
-                <a href="#" className="text-gray-500 hover:text-gray-900">About</a>
-                <a href="#" className="text-gray-500 hover:text-gray-900">Contact</a>
-            </div>
-    </div>
+            />
 
-  )
-}
+            
+        </Link>
 
-export default Navbar
+        <div className="margin mx-auto max-w-2xl  grow">
+          <Input placeholder="Search" />
+        </div>
+        <div className=" margin mx-auto inline-flex flex-row gap-2 items-center ml-5">
+          <Button variant="outline">Login</Button>
+          <Button variant="outline">Sign Up</Button>
+          <Button
+            onClick={() =>
+              setTheme(theme === 'dark' ? 'light' : 'dark')
+            }
+            variant="ghost"
+          >
+          
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                className="w-5 h-5 text-gray-800 dark:text-gray-200"
+              >
+                {theme === 'dark' ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                )}
+              </svg>
+          
+          </Button>
+
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;

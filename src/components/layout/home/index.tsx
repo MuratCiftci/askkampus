051ffd1c1361c14187a91de33707c2d71db26793
MuratCiftci <mciftci174@posta.pau.edu.tr>
2023-03-18 14:34:30 +0300

    style: change color and padding

--- src/components/layout/home/index.tsx
+++ src/components/layout/home/index.tsx
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] pl-64">
+    <main className="flex min-h-screen flex-col items-center justify-center pl-64 py-12">
       <Navbar />
       <Sidebar />
       {children}

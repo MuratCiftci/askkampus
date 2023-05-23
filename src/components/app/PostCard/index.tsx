e6b519f06fb6f0cbaad1245e907042edb6cc507d
MuratCiftci <mciftci174@posta.pau.edu.tr>
2023-05-24 00:46:12 +0300

    feature: new post card

--- src/components/app/PostCard/index.tsx
+++ src/components/app/PostCard/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { type Post } from "./index.type";
-import { ArrowBigDown, ArrowBigUp } from "lucide-react";
+import { ArrowBigDown, ArrowBigUp, ArrowUp } from "lucide-react";
 import { Button } from "~/components/shared/ui/Button";
 import Image from "next/image";
 import Link from "next/link";
@@ -30,72 +30,78 @@ const PostCard = (props: Props) => {
         description: err.message,
         variant: "destructive",
       });
-    }
+    },
   });
 
   return (
-    <div className="relative mb-4 min-h-[300px] cursor-pointer rounded-sm border border-gray-300 border-opacity-50 p-2 pl-10 hover:border-opacity-100">
-      <div className="absolute left-0 top-0 flex w-10 flex-col items-center justify-center border-l border-transparent py-2 pr-2">
-        <div className="flex flex-col items-center  fill-inherit">
-          <Button
-            variant="ghost"
-            size="sm"
-            className="hover:bg-inherit hover:text-blue-500 dark:hover:bg-inherit dark:hover:text-blue-500"
+    <article className="break-inside mb-4 flex flex-col rounded-xl bg-white  bg-clip-border p-4 dark:bg-slate-800 dark:bg-opacity-10">
+      <div className="flex items-center justify-between pb-4">
+        <div className="flex w-10 flex-col items-center justify-center border-l border-transparent   pr-2">
+          <ArrowBigUp
+            size={20}
+            strokeWidth={2.4}
+            className="m-0 cursor-pointer hover:bg-inherit hover:text-blue-500 dark:hover:bg-inherit dark:hover:text-blue-500"
             onClick={() => {
               upvotePost.mutate({ postId: post.id });
             }}
-          >
-            <ArrowBigUp />
-          </Button>
-          <span className="text-xs font-bold text-gray-500">
+          />
+
+          <span className="m-0 p-0 text-xs font-bold text-gray-500">
             {" "}
             {upvoteCount}{" "}
           </span>
 
-          <Button
-            variant="ghost"
-            size="sm"
-            className="hover:bg-inherit hover:text-orange-500 dark:hover:bg-inherit dark:hover:text-orange-500"
-          >
-            <ArrowBigDown />
-          </Button>
+          <ArrowBigDown
+            size={20}
+            strokeWidth={2.4}
+            className="w-24px h-24px cursor-pointer hover:bg-inherit hover:text-orange-500 dark:hover:bg-inherit dark:hover:text-orange-500"
+          />
         </div>
-      </div>
-      <div className="w-full">
-        <div className="flex items-center justify-start">
-          {post.community && post.community.name && (
-            <Link href={`/c/${post.community.name}`}>
-              {post.community.name}
-            </Link>
-          )}
-          <span className="mx-1 text-xs font-bold text-gray-500">•</span>
-          {post.user && post.user.name && (
-            <Link href={`/u/${post.user.name}`}>
-              u/{post.user.name}{" "}
-              <span className="text-xs  text-gray-500">tarafından</span>
-            </Link>
-          )}
-          <span className="mx-1 text-xs font-bold text-gray-500">•</span>
-          <span className="text-xs font-bold text-gray-500">
+        <a className="inline-block" href="#">
+          <Image
+            className="h-14 w-14 max-w-none rounded-full"
+            src={post.community.image_url || "/images/placeholder-avatar.jpg"}
+            alt="Profile image"
+            width={56}
+            height={56}
+          />
+        </a>
+        <div className="ml-4 flex flex-grow flex-col">
+          <div className="flex flex-wrap items-center justify-between">
+            <a className="mr-2 inline-block text-sm font-bold" href="#">
+              /{post.community.name}
+            </a>
+            <span className="mx-1 text-xs text-slate-500 dark:text-slate-300">
+              {" "}
+              Posted by {post.user.name}
+            </span>{" "}
+          </div>
+          <div className="flex flex-col items-start text-xs text-slate-500 dark:text-slate-300">
             {getTimeDifference(post.createdAt)}
-          </span>
-        </div>
-
-        <div className="mx-4" />
-        <Link href={`/p/${post.id}`}>
-          <h1 className="text-xl font-bold text-gray-800 dark:text-gray-200">
-            {post.title}
-          </h1>
-        </Link>
-        <div className="mt-2">
-          <Link href={`/p/${post.id}`}>
-            <p className="text-sm text-gray-800 dark:text-gray-200">
-              {post.body}
-            </p>
-          </Link>
+          </div>
         </div>
       </div>
-    </div>
+      <h2 className="text-lg font-bold   tracking-tight text-slate-900 dark:text-slate-100">
+        <Link href={`/post/${post.id}`}>{post.title}</Link>
+      </h2>
+      <div className="py-4">
+        {post.image_url && (
+          <div className="relative h-48">
+            <Image
+              className="rounded-xl object-contain"
+              src={post.image_url}
+              alt="Post image"
+              layout="fill"
+            />
+          </div>
+        )}
+      </div>
+      <p>
+        {typeof post.body === "string" && post.body.length > 200
+          ? post.body.slice(0, 200) + "..."
+          : post.body}
+      </p>
+    </article>
   );
 };
 
